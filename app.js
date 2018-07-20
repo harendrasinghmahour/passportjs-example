bc8373cb2a1b04405afcc580054ec0fb560bbc5f
harry <harendrasinghmahour@onepawaway.com>
2018-07-20 19:26:52 +0530

    init

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const googleSetup = require('./config/passport-google');
+const facebookSetup = require('./config/passport-facebook');
+const twitterSetup = require('./config/passport-twitter');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+const app = express();
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// set up session cookies
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/passport', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+// set up routes
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+    res.render('home', { user: req.user });
+});
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
---
+++ config/keys.js
@@ -0,0 +1,19 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: "",
+        clientSecret: ""
+    },
+    facebook: {
+        clientID: "",
+        clientSecret: ""
+    },
+    twitter: {
+        clientID: "",
+        clientSecret: ""
+    },
+        session: {
+            cookieKey: 'harryjerry'
+        }
+};
